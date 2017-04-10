f560b80f619b6a5138f99758e63c0c24ad584b89
Mathieu ARQUILLIERE <Mathieu ARQUILLIERE>
2017-04-10 16:35:10 +0200

    Music and SoundEffect

---
+++ Game.js
@@ -0,0 +1,22 @@
+class Game {
+	constructor(){
+		var music = new Music("resources/sounds/musics/", 3);
+		var soundEffect = new SoundEffect("resources/chapters/");
+	}
+
+	load(){
+		music.load();
+	}
+
+	update(){
+
+	}
+
+	render(){
+
+	}
+
+	selectLevel(){
+
+	}
+}
---
+++ sound/SoundEffect.js
@@ -0,0 +1,40 @@
+class SoundEffect{
+	constructor(path){
+		this.nblvl = 10;
+		this.nbchap = 1;
+		this.path = path;
+		this.pathArray = new Array();
+		this.soundArray = new Array();
+
+		for (var i=0; i<this.nblvl; i++){
+			this.pathArray.push(new Array());
+			this.soundArray.push(new Array());
+		}
+		for (var i=0; i<this.nbchap; i++){
+			for (var j=0; j<this.nblvl; j++)
+				this.pathArray[i].push(this.path + String(i) + "/" + "lvl_" + String(j) + ".mp3");
+		}
+	}
+
+	load(){
+		for(var i=0; i<this.nbchap; i++){
+			for(var j=0; j<this.nblvl; j++){
+				this.soundArray[i].push(loadSound(this.pathArray[i][j]));
+			}
+		}
+	}
+
+	volume(newVolume){
+		for(var i=0; i<this.nbSound; i++){
+			for(var j=0; j<this.nblvl; j++){
+				this.soundArray[i][j].setVolume(newVolume);
+			}
+		}
+	}
+
+	play(nchapter, nlvl){
+		if (!this.soundArray[nchapter][nlvl].isPlaying()){
+			this.soundArray[nchapter][nlvl].play();
+		}
+	}
+}
