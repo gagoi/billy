95005612dd6b7fa87d34042a362ae9a9c70e79d9
FixyNeko <felix@dhumes.fr>
2017-04-10 16:45:11 +0200

    matter.js integration

--- Game.js
+++ Game.js
@@ -1,19 +1,25 @@
 class Game {
 	constructor(){
-		var music = new Music("resources/sounds/musics/", 3);
-		var soundEffect = new SoundEffect("resources/chapters/");
+		this.music = new Music("resources/sounds/musics/", 3);
+		this.soundEffect = new SoundEffect("resources/chapters/");
+
+		this.engine = Matter.Engine.create();
+
+		this.entities = [];
 	}
 
 	load(){
-		music.load();
+		this.music.load();
 	}
 
 	update(){
-
+		Matter.Engine.run(this.engine);
 	}
 
 	render(){
-
+		for( var entity in this.entities) {
+			entity.render();
+		}
 	}
 
 	selectLevel(){
