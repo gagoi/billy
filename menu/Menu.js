f20a2d0c42343678c85e7d475ec44d3954b009f8
wiyochi <mat.arq@sfr.fr>
2017-04-13 21:10:22 +0200

    amelio menu (level selection)

--- menu/Menu.js
+++ menu/Menu.js
@@ -1,8 +1,7 @@
 class Menu{
-	constructor(type){
-		this.menuType = type;
+	constructor(){
 		this.whichMenu = 0;
-		this.nChapter = 2;
+		this.nChapter = 3;
 		this.nLevel = 10*this.nChapter;
 		this.levelArray = new Array();
 
@@ -10,7 +9,7 @@ class Menu{
 		//Access : levelArray[10*(chapter-1)+(level)]
 		//Reverse:   i:   chap=(i/10)+1  level=(i%10)
 		for (var i=0; i<this.nLevel; i++){
-			this.levelArray.push(new MenuButton(2, (Math.floor(i/10))+1,  i%10, 20+(i%5)*200, 50+(Math.floor(i/5))*100, 150, 20));
+			this.levelArray.push(new MenuButton(2, (Math.floor(i/10))+1,  i%10, 355+(i%10)*66, 36+(Math.floor(i/10))*182, 60, 140));
 		}
 
 		this.bNewGame = new MenuButton(0, -1, -1, 312, 294, 400, 100);
@@ -82,53 +81,7 @@ class Menu{
 				break;
 		}
 	}
-
-	//getType(){
-	//	return this.menuType;
-	//}
 }
-
-/*
-class MainMenu extends Menu{
-	constructor(type){
-		super(type);
-		this.b1 = new MenuButton("newGameButton", 300, 200, 200, 50);
-		this.b2 = new MenuButton("otherMenu", 300, 300, 200, 50);
-	}
-
-	mouse(x, y){
-		this.b1.mousePressed(x, y);
-		this.b2.mousePressed(x, y);
-	}
-
-	update(){
-
-	}
-}
-
-class LevelMenu extends Menu{
-	constructor(){
-		this.nbChap = 2;
-		this.nbLvl = 10;
-		this.buttonArray = new Array();
-		for (var i=0; i<this.nbChap; i++){
-			this.buttonArray.push(new Array());
-			for (var j=0; j<this.nbLvl; j++){
-				var str = "Chapter " + String(i) + " Level " + String(j)
-				this.buttonArray[i].push(new MenuButtonLevel("levelButton", str, 20+i*200, 50+j*110, 150, 20, i, j));
-			}
-		}
-	}
-
-	mouse(x, y){
-		for (var i=0; i<this.nbChap; i++){
-			for (var j=0; j<this.nbLvl; j++){
-				this.buttonArray[i][j].mousePressed(x, y);
-			}
-		}
-	}
-}
-*/
 
 
 
